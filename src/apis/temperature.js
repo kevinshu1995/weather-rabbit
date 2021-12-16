1ac46a1a725ef5c93b5a2b8011a46eb714b12857
kevin_hws <kevinshu1995@gmail.com>
2021-12-17 01:59:21 +0800

    refactor: 💡 海拔排列低溫 重構

--- src/apis/temperature.js
+++ src/apis/temperature.js
@@ -7,38 +7,87 @@ export async function Temperature() {
         elementName: "TEMP,ELEV",
     });
 
-    const lowestLocation = data.records.location.reduce((result, currentLocation) => {
-        const currentTemp = getElementValueByKey(currentLocation.weatherElement, "TEMP");
-
-        currentLocation = {
+    /**
+     *  * 調整資料結構
+     *  {Array}
+     */
+    const refactorData = data.records.location.map(item => {
+        const temp = Number(getElementValueByKey(item.weatherElement, "TEMP"));
+        const elev = Number(getElementValueByKey(item.weatherElement, "ELEV"));
+        return {
             latLon: {
-                lat: currentLocation.lat,
-                lon: currentLocation.lon,
+                lat: item.lat,
+                lon: item.lon,
             },
             location: {
-                name: currentLocation.locationName,
-                city: getParameterValueByKey(currentLocation.parameter, "CITY"),
-                town: getParameterValueByKey(currentLocation.parameter, "TOWN"),
+                name: item.locationName,
+                city: getParameterValueByKey(item.parameter, "CITY"),
+                town: getParameterValueByKey(item.parameter, "TOWN"),
             },
-            time: currentLocation.time && currentLocation.time.obsTime,
-            temperature: currentTemp,
+            time: item.time && item.time.obsTime,
+            temperature: temp,
+            locationElev: elev,
         };
+    });
+
+    /**
+     *  * 最低溫的地區
+     *  {Object}
+     */
+    const lowestLocation = refactorData.reduce((result, currentLocation) => {
+        const currentTemp = currentLocation.temperature;
 
         if (Object.keys(result).length === 0) return currentLocation;
+        if (Number(currentTemp) === -99) return result;
 
-        if (currentTemp === undefined || Number(currentTemp) === -99) {
-            return result;
-        } else {
-            return Number(currentTemp) > Number(result.temperature) ? result : currentLocation;
-        }
+        return Number(currentTemp) > Number(result.temperature) ? result : currentLocation;
     }, {});
 
-    // TODO 依照海拔排列
-    // const lowestLocationByElev = null
+    /**
+     *  * 各海拔最低溫地區
+     *  {Object}
+     */
+    const lowestLocationByElev = refactorData.reduce((all, current) => {
+        const tempCurrent = current.temperature;
+        const elevCurrent = current.locationElev;
+        const level = Math.floor(elevCurrent / 500) * 500;
+
+        if (isNaN(tempCurrent) || isNaN(elevCurrent)) return all;
+        if (current.temperature === -99) return all;
+
+        if (all[level] === undefined) {
+            all[level] = current;
+            return all;
+        }
+
+        const tempInAll = all[level].temperature;
+        if (tempInAll > tempCurrent) {
+            all[level] = current;
+        } else if (tempInAll === tempCurrent) {
+            // * 同溫度
+            all[level].sameTemp =
+                all[level].sameTemp === undefined ? [all[level], current] : [...all[level].sameTemp, current];
+        }
+        return all;
+    }, {});
+
+    /**
+     *  * 各海拔排列，最低溫的地區
+     *  {Array}
+     */
+    const sortedLowestLocationByElev = Object.entries(lowestLocationByElev)
+        .map(([elev, item]) => {
+            return { ...item, elev };
+        })
+        .sort((prev, next) => {
+            return prev.elev - next.elev;
+        });
 
     return {
         data,
         status,
         lowestLocation,
+        lowestLocationByElev,
+        sortedLowestLocationByElev,
     };
 }
--- src/components/LowTempByElev.vue
+++ src/components/LowTempByElev.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="text-gray-900">
-        <ul v-if="sortTempByElev" class="space-y-4">
-            <template v-for="location in sortTempByElev">
-                <li :key="`sorted-location-with-elev-${location.stationId}`" class="flex flex-wrap gap-y-2">
+        <ul v-if="sortedLowestLocationByElev && status === 200" class="space-y-4">
+            <template v-for="(location, i) in sortedLowestLocationByElev">
+                <li :key="`sorted-location-with-elev-${i}`" class="flex flex-wrap gap-y-2">
                     <h3 class="w-full text-xs font-bold">
                         {{ location.elev }} ~ {{ Number(location.elev) + 499 }} 公尺
                     </h3>
                     <p class="w-36 flex gap-2">
                         <span class="text-5xl font-bold">
-                            {{ getElementValueByKey(location.weatherElement, "TEMP") }}
+                            {{ location.temperature }}
                         </span>
                         <span class="text-sm self-end">°C</span>
                     </p>
                     <div class="flex flex-col justify-between">
-                        <p class="text-sm text-gray-700">
-                            {{ getElementValueByKey(location.weatherElement, "ELEV") }} 公尺
-                        </p>
+                        <p class="text-sm text-gray-700">{{ location.locationElev }} 公尺</p>
                         <p class="font-bold text-xl">
-                            {{ locationName(location.parameter) }} {{ location.locationName }}
+                            {{ location.location.city }}
+                            {{ location.location.town }}
+                            {{ location.location.name }}
                         </p>
                     </div>
                 </li>
@@ -27,84 +27,28 @@
 </template>
 
 <script>
-import { weatherCurrentRecord } from "@/apis/weather.js";
+import { Temperature } from "@/apis/temperature.js";
 
 export default {
     name: "LowTempByElev",
 
     data() {
         return {
-            weatherData: null,
+            sortedLowestLocationByElev: null,
+            status: null,
         };
     },
 
     async mounted() {
-        this.getTempAndElev();
-    },
-
-    computed: {
-        tempByElev() {
-            if (!this.weatherData) return null;
-            const getValue = (ary, key) => this.getElementValueByKey(ary, key);
-            return this.weatherData.reduce((all, current) => {
-                const tempCurrent = Number(getValue(current.weatherElement, "TEMP"));
-                const elevCurrent = Number(getValue(current.weatherElement, "ELEV"));
-                if (isNaN(tempCurrent) || isNaN(elevCurrent)) return all;
-
-                const level = Math.floor(elevCurrent / 500) * 500;
-
-                if (Number(tempCurrent) !== -99) {
-                    if (all[level] === undefined) {
-                        all[level] = current;
-                    } else {
-                        const tempInAll = Number(getValue(all[level].weatherElement, "TEMP"));
-                        if (tempInAll > tempCurrent) {
-                            all[level] = current;
-                        } else if (tempInAll === tempCurrent) {
-                            console.log("居然有同溫度的...");
-                            all[level].sameTemp =
-                                all[level].sameTemp === undefined
-                                    ? [all[level], current]
-                                    : [...all[level].sameTemp, current];
-                        }
-                    }
-                }
-
-                return all;
-            }, {});
-        },
-        sortTempByElev() {
-            if (!this.tempByElev) return null;
-            return Object.entries(this.tempByElev)
-                .map(([elev, item]) => {
-                    return { ...item, elev };
-                })
-                .sort((prev, next) => {
-                    return prev.elev - next.elev;
-                });
-        },
+        this.getData();
     },
 
     methods: {
-        async getTempAndElev() {
-            const { data } = await weatherCurrentRecord({
-                elementName: "TEMP,ELEV",
-            });
-            this.weatherData = data.records.location;
-            return data.records.location;
-        },
-
-        getElementValueByKey(ary, key) {
-            if (!Array.isArray(ary)) return null;
-            return ary.find(item => item.elementName === key).elementValue;
-        },
-
-        getParamByKey(ary, key) {
-            if (!Array.isArray(ary)) return null;
-            return ary.find(item => item.parameterName === key).parameterValue;
-        },
-        locationName(parameter) {
-            return `${this.getParamByKey(parameter, "CITY")} ${this.getParamByKey(parameter, "TOWN")}`;
+        async getData() {
+            const { lowestLocationByElev, sortedLowestLocationByElev, status } = await new Temperature();
+            this.sortedLowestLocationByElev = sortedLowestLocationByElev;
+            this.status = status;
+            console.log("第二題", lowestLocationByElev);
         },
     },
 };
