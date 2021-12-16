d9171a5eabe73c194ecd5d368050a7ae9381982f
kevin_hws <kevinshu1995@gmail.com>
2021-12-16 17:32:50 +0800

    refactor: 💡 最低溫 重構

---
+++ src/apis/temperature.js
@@ -0,0 +1,47 @@
+import { weatherCurrentRecord } from "./weather.js";
+
+const getElementValueByKey = (obj, key) => obj.find(element => element.elementName === key).elementValue;
+const getParameterValueByKey = (obj, key) => obj.find(element => element.parameterName === key).parameterValue;
+
+export async function Temperature() {
+    const { data, status } = await weatherCurrentRecord({
+        elementName: "TEMP,ELEV",
+    });
+
+    const _lowestLocation = data.records.location.reduce((result, currentLocation) => {
+        const currentTemp = getElementValueByKey(currentLocation.weatherElement, "TEMP");
+
+        currentLocation = {
+            ...currentLocation,
+            temperature: currentTemp,
+        };
+
+        if (Object.keys(result).length === 0) return currentLocation;
+
+        if (currentTemp === undefined || Number(currentTemp) === -99) {
+            return result;
+        } else {
+            return Number(currentTemp) > Number(result.temperature) ? result : currentLocation;
+        }
+    }, {});
+
+    const lowestLocation = {
+        latLon: {
+            lat: _lowestLocation.lat,
+            lon: _lowestLocation.lon,
+        },
+        location: {
+            name: _lowestLocation.locationName,
+            city: getParameterValueByKey(_lowestLocation.parameter, "CITY"),
+            town: getParameterValueByKey(_lowestLocation.parameter, "TOWN"),
+        },
+        time: _lowestLocation.time && _lowestLocation.time.obsTime,
+        temperature: _lowestLocation.temperature,
+    };
+
+    return {
+        data,
+        status,
+        lowestLocation,
+    };
+}
--- src/components/LowestTemp.vue
+++ src/components/LowestTemp.vue
@@ -1,25 +1,24 @@
 <template>
     <div class="text-gray-900">
-        <div v-if="location" class="flex flex-wrap gap-x-10 gap-y-2">
+        <div v-if="data && status === 200" class="flex flex-wrap gap-x-10 gap-y-2">
             <div class="flex justify-between w-full">
                 <h2 class="text-gray-900 font-bold text-xs">最低溫觀測站</h2>
-                <p class="text-xs text-gray-400">{{ location.time.obsTime }}</p>
+                <p class="text-xs text-gray-400">{{ lowestLocation.time }}</p>
             </div>
             <div class="self-end">
                 <div class="flex gap-2">
-                    <p class="text-5xl font-bold">{{ getElementValueByKey(location.weatherElement, "TEMP") }}</p>
+                    <p class="text-5xl font-bold">{{ lowestLocation.temperature }}</p>
                     <span class="self-end">°C</span>
                 </div>
             </div>
             <div class="space-y-1 self-end">
-                <p class="text-xs text-gray-700">經緯 {{ location.lat }}, {{ location.lon }}</p>
+                <p class="text-xs text-gray-700">
+                    經緯 {{ lowestLocation.latLon.lat }}, {{ lowestLocation.latLon.lon }}
+                </p>
                 <h2 class="font-bold text-xl">
-                    <template v-for="param in locationName">
-                        <span :key="`location-name-${param.parameterName}`">
-                            {{ param.parameterValue }}
-                        </span>
-                    </template>
-                    <span>{{ location.locationName }}</span>
+                    <span>{{ lowestLocation.location.city }}</span>
+                    <span>{{ lowestLocation.location.town }}</span>
+                    <span>{{ lowestLocation.location.name }}</span>
                 </h2>
             </div>
         </div>
@@ -30,57 +29,28 @@
 </template>
 
 <script>
-import { weatherCurrentRecord } from "@/apis/weather.js";
+import { Temperature } from "@/apis/temperature.js";
 export default {
     name: "Lowest-Temp",
 
     data() {
         return {
-            location: null,
+            data: null,
+            status: null,
+            lowestLocation: null,
         };
     },
 
-    async mounted() {
-        this.location = await this.getLowestTemp();
-    },
-
-    computed: {
-        locationName() {
-            if (!this.location) return [];
-            return this.location.parameter.filter(
-                item => item.parameterName === "CITY" || item.parameterName === "TOWN"
-            );
-        },
+    mounted() {
+        this.getTemperature();
     },
 
     methods: {
-        async getLowestTemp() {
-            try {
-                const { data } = await weatherCurrentRecord({
-                    elementName: "TEMP",
-                });
-                const findTempObj = location => location.weatherElement.find(element => element.elementName === "TEMP");
-                const lowestLocation = data.records.location.reduce((result, currentLocation) => {
-                    if (Object.keys(result).length === 0) return currentLocation;
-                    const currentTemp = findTempObj(currentLocation);
-                    const resultTemp = findTempObj(result);
-                    if (currentTemp === undefined || Number(currentTemp.elementValue) === -99) {
-                        return result;
-                    } else {
-                        return Number(currentTemp.elementValue) > Number(resultTemp.elementValue)
-                            ? result
-                            : currentLocation;
-                    }
-                }, {});
-                return lowestLocation;
-            } catch (error) {
-                console.error("get lowestTemp failed", error);
-            }
-        },
-
-        getElementValueByKey(ary, key) {
-            if (!Array.isArray(ary)) return null;
-            return ary.find(item => item.elementName === key).elementValue;
+        async getTemperature() {
+            const { data, status, lowestLocation } = await new Temperature();
+            this.data = data;
+            this.status = status;
+            this.lowestLocation = lowestLocation;
         },
     },
 };
