d1410e380d5821b35a53fa0c630b8b2a9fe701cc
kevin_hws <kevinshu1995@gmail.com>
2021-12-19 23:52:00 +0800

    refactor: 💡 第四題 layout

--- src/components/WeeklyWeather.vue
+++ src/components/WeeklyWeather.vue
@@ -1,34 +1,45 @@
 <template>
     <div>
-        <div v-if="status === 200">
-            <div class="flex gap-4 items-center">
-                <h3 class="font-bold">選一下啦~</h3>
+        <div v-if="status === 200" class="space-y-4">
+            <div class="flex gap-4 items-center w-full">
                 <select
                     name="location"
                     id="location"
-                    class="px-4 py-2 rounded-lg"
+                    class="px-4 py-2 cursor-pointer rounded-lg w-full"
                     v-if="locationOptions"
                     v-model="form.selectedLocation"
                     @change="onChangeLocation"
                 >
-                    <option value="" disabled>--- 點我! ---</option>
+                    <option value="" disabled>選啦！選啦！</option>
                     <option v-for="(option, i) in locationOptions" :value="option.value" :key="`location-option-${i}`">
                         {{ option.text }}
                     </option>
                 </select>
             </div>
             <template v-if="result">
-                <div>
-                    <h4>最高溫</h4>
-                    <p>{{ result.highestTemp }}</p>
-                </div>
-                <div>
-                    <h4>最低溫</h4>
-                    <p>{{ result.lowestTemp }}</p>
-                </div>
-                <div>
-                    <h4>最大溫差</h4>
-                    <p>{{ result.maxDiff.temp.diff }}</p>
+                <h3 class="font-bold">未來一周溫度預測</h3>
+                <div class="flex gap-8">
+                    <div>
+                        <h4 class="text-sm">最高溫</h4>
+                        <p class="text-5xl font-bold">
+                            {{ result.highestTemp }}
+                            <span class="text-sm self-end">°C</span>
+                        </p>
+                    </div>
+                    <div>
+                        <h4 class="text-sm">最低溫</h4>
+                        <p class="text-5xl font-bold">
+                            {{ result.lowestTemp }}
+                            <span class="text-sm self-end">°C</span>
+                        </p>
+                    </div>
+                    <div>
+                        <h4 class="text-sm">單日最大溫差</h4>
+                        <p class="text-5xl font-bold">
+                            {{ result.maxDiff.temp.diff }}
+                            <span class="text-sm self-end">°C</span>
+                        </p>
+                    </div>
                 </div>
             </template>
         </div>
@@ -79,7 +90,7 @@ export default {
             console.log(
                 `第四題: 最高溫 ${highestTemp} 度 | 最低溫 ${lowestTemp} 度 | 最大溫差 ${maxDiff.temp.diff} 度`
             );
-            console.log(maxDiff);
+            console.log("最大溫差的資料", maxDiff);
         },
     },
 };
