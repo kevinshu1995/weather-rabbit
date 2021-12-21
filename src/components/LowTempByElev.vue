13a39138c2fbd24aa9e83ba01a759077d3a2a467
kevin_hws <kevinshu1995@gmail.com>
2021-12-21 21:49:24 +0800

    refactor: 💡 多餘的 template tag

--- src/components/LowTempByElev.vue
+++ src/components/LowTempByElev.vue
@@ -1,27 +1,27 @@
 <template>
     <div class="text-gray-900">
         <ul v-if="sortedLowestLocationByElev && status === 200" class="space-y-4">
-            <template v-for="(location, i) in sortedLowestLocationByElev">
-                <li :key="`sorted-location-with-elev-${i}`" class="flex flex-wrap gap-y-2">
-                    <h3 class="w-full text-xs font-bold">
-                        {{ location.elev }} ~ {{ Number(location.elev) + 499 }} 公尺
-                    </h3>
-                    <p class="w-36 flex gap-2">
-                        <span class="text-5xl font-bold">
-                            {{ location.temperature }}
-                        </span>
-                        <span class="text-sm self-end">°C</span>
+            <li
+                v-for="(location, i) in sortedLowestLocationByElev"
+                :key="`sorted-location-with-elev-${i}`"
+                class="flex flex-wrap gap-y-2"
+            >
+                <h3 class="w-full text-xs font-bold">{{ location.elev }} ~ {{ Number(location.elev) + 499 }} 公尺</h3>
+                <p class="w-36 flex gap-2">
+                    <span class="text-5xl font-bold">
+                        {{ location.temperature }}
+                    </span>
+                    <span class="text-sm self-end">°C</span>
+                </p>
+                <div class="flex flex-col justify-between">
+                    <p class="text-sm text-gray-700">{{ location.locationElev }} 公尺</p>
+                    <p class="font-bold text-xl">
+                        {{ location.location.city }}
+                        {{ location.location.town }}
+                        {{ location.location.name }}
                     </p>
-                    <div class="flex flex-col justify-between">
-                        <p class="text-sm text-gray-700">{{ location.locationElev }} 公尺</p>
-                        <p class="font-bold text-xl">
-                            {{ location.location.city }}
-                            {{ location.location.town }}
-                            {{ location.location.name }}
-                        </p>
-                    </div>
-                </li>
-            </template>
+                </div>
+            </li>
         </ul>
     </div>
 </template>
